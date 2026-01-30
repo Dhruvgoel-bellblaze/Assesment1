d4798568131fbc9ba36eb8b1d6f7a26bd63f4a38
Dhruvgoel-bellblaze <dhruv.goel@bellblazetech.com>
2026-01-30 09:47:59 +0530

    Add files via upload

---
+++ Day-1_task.py
@@ -0,0 +1,19 @@
+nums = [15, 4, 28, 6, 10]
+
+nums.insert(2, 99)
+print(nums)
+
+nums.pop(1)
+print(nums)
+
+nums.append(42)
+print(nums)
+
+nums.sort()
+print(nums)
+
+nums.pop()
+print(nums)
+
+nums.reverse()
+print(nums)
